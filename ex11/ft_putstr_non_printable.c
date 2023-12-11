edf7a632c72da70176b14b23a75020a18109a050
Davide Mattia Palmese <dpalmese@e3r8p3.42roma.it>
2023-12-11 16:26:57 +0100

    removed ex10 and added norm

--- ex11/ft_putstr_non_printable.c
+++ ex11/ft_putstr_non_printable.c
@@ -1,33 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr_non_printable.c                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpalmese <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 16:25:30 by dpalmese          #+#    #+#             */
+/*   Updated: 2023/12/11 16:26:12 by dpalmese         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void    print_hex(char c)
+void	print_hex(char c)
 {
-    char    *hex_alphabet;
-    char    hex[2];
+	char	*hex_alphabet;
+	char	hex[2];
 
-    hex_alphabet = "0123456789abcdef";
-    hex[0] = hex_alphabet[(c >> 4) & 0xF];
-    hex[1] = hex_alphabet[c & 0xF];
-
-    write(1, hex, 2);
+	hex_alphabet = "0123456789abcdef";
+	hex[0] = hex_alphabet[(c >> 4) & 0xF];
+	hex[1] = hex_alphabet[c & 0xF];
+	write(1, hex, 2);
 }
 
-void    ft_putstr_non_printable(char *str)
+void	ft_putstr_non_printable(char *str)
 {
-    while(*str)
-    {
-        if (*str >= '\a' && *str <= '\r')
-        {
-            write(1, "\\", 1);
-            print_hex(*str);
-        }
-        else
-            write(1, str, 1);
-        str++;
-    }
-}
-
-int main(){
-    ft_putstr_non_printable("Coucou\ntu vas bien ?");
-    return 0;
+	while (*str)
+	{
+		if (*str >= '\a' && *str <= '\r')
+		{
+			write(1, "\\", 1);
+			print_hex(*str);
+		}
+		else
+			write(1, str, 1);
+		str++;
+	}
 }
