e1497a9cd43b79948f9c36754acdc372ac730c91
Davide Mattia Palmese <dpalmese@e3r8p7.42roma.it>
2023-12-11 12:21:27 +0100

    added norm

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -1,16 +1,35 @@
-unsigned int ft_strlcpy(char *dst, const char *src, unsigned int size)
-{
-    const char *s = src;
-	unsigned char *d = (unsigned char *)dst;
-    unsigned int n = size;
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpalmese <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 10:35:56 by dpalmese          #+#    #+#             */
+/*   Updated: 2023/12/11 10:44:08 by dpalmese         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-    while (n--)
-    {
-        if ((*d++ = *s++) == '\0'){
-        break;
+unsigned int	ft_strlcpy(char *dst, const char *src, unsigned int size)
+{
+	const char			*s;
+	unsigned char		*d;
+	unsigned int		n;
+
+	*s = src;
+	d = (unsigned char *)dst;
+	n = size;
+	while (n--)
+	{
+		if (*s == '\0')
+		{
+			break;
         }
-    }
-    if (n == 0) {
+		*d = *s;
+		d++;
+		s++;
+	}
+	if (n == 0) {
 		if (size != 0)
 			*d = '\0';
 		while (*s++);
