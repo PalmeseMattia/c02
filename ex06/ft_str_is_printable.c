4fc96461577deeac4fd9388462421250f549bda9
Davide Mattia Palmese <dpalmese@e3r8p3.42roma.it>
2023-12-11 10:13:28 +0100

    added norm

--- ex06/ft_str_is_printable.c
+++ ex06/ft_str_is_printable.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_printable.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpalmese <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 10:11:03 by dpalmese          #+#    #+#             */
+/*   Updated: 2023/12/11 10:11:54 by dpalmese         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_isprint(int c)
 {
-    if (c >= ' ' && c <= '~')
+	if (c >= ' ' && c <= '~')
 		return (1);
 	else
 		return (0);
@@ -8,10 +20,11 @@ int	ft_isprint(int c)
 
 int	ft_str_is_printable(char *str)
 {
-	while(*str)
+	while (*str)
 	{
-		if(!ft_isprint(*str))
+		if (!ft_isprint(*str))
 			return (0);
+		str++;
 	}
 	return (0);
 }
--- ex07/ft_strupcase.c
+++ ex07/ft_strupcase.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strupcase.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpalmese <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 10:12:20 by dpalmese          #+#    #+#             */
+/*   Updated: 2023/12/11 10:12:54 by dpalmese         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_toupper(int c)
 {
-    if (c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (c - 32);
 	else
-		return c;
+		return (c);
 }
 
 char	*ft_strupcase(char *str)
@@ -11,9 +23,10 @@ char	*ft_strupcase(char *str)
 	int	n;
 
 	n = 0;
-	while(str[n])
+	while (str[n])
 	{
 		str[n] = ft_toupper(str[n]);
+		str++;
 	}
 	return (str);
 }
