79e71aa85986449d5cb2c6af8e68eee2ef4d6b30
Davide Mattia Palmese <dpalmese@e3r8p3.42roma.it>
2023-12-11 10:14:32 +0100

    added norm

--- ex08/ft_strlowcase.c
+++ ex08/ft_strlowcase.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpalmese <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 10:13:40 by dpalmese          #+#    #+#             */
+/*   Updated: 2023/12/11 10:14:15 by dpalmese         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_tolower(int c)
 {
-    if (c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (c + 32);
 	else
-		return c;
+		return (c);
 }
 
 char	*ft_strlowcase(char *str)
@@ -11,9 +23,10 @@ char	*ft_strlowcase(char *str)
 	int	n;
 
 	n = 0;
-	while(str[n])
+	while (str[n])
 	{
 		str[n] = ft_tolower(str[n]);
+		str++;
 	}
 	return (str);
 }
