10221ab23572e0f157776fae91b56df47a7797d5
Davide Mattia Palmese <dpalmese@e3r8p3.42roma.it>
2023-12-11 10:29:12 +0100

    added norm

--- ex09/ft_strcapitalize.c
+++ ex09/ft_strcapitalize.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpalmese <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 10:14:44 by dpalmese          #+#    #+#             */
+/*   Updated: 2023/12/11 10:29:01 by dpalmese         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	isalpha(int c)
 {
 	if ((c >= 'A' && c <= 'Z') || c >= 'a' && c <= 'z')
@@ -5,43 +17,49 @@ int	isalpha(int c)
 	return (0);
 }
 
-int isdigit(int c)
+int	isdigit(int c)
 {
-    if (c >= '0' && c <= '9')
+	if (c >= '0' && c <= '9')
 		return (1);
 	return (0);
 }
 
-int tolower(int c)
+int	tolower(int c)
 {
-    if (c >= 'A' && c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 		return (c + 32);
 	else
-		return c;
+		return (c);
 }
 
-int toupper(int c)
+int	toupper(int c)
 {
-    if (c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (c - 32);
 	else
 		return (c);
 }
 
-void capitalizeWords(char *str) {
-    int capitalize = 1;
+void	ft_strcapitalize(char *str)
+{
+	int	capitalize;
 
-    while (*str) {
-        if (isalpha(*str) && capitalize) {
-            *str = toupper(*str);
-            capitalize = 0;
-        }
-        else if (isalpha(*str) && !capitalize) {
-            *str = tolower(*str);
-        }
-        else if (!(isdigit(*str) || isalpha(*str))) {
-            capitalize = 1;
-        }
+	capitalize = 1;
+	while (*str)
+	{
+		if (isalpha(*str) && capitalize)
+		{
+			*str = toupper(*str);
+			capitalize = 0;
+		}
+		else if (isalpha(*str) && !capitalize)
+		{
+			*str = tolower(*str);
+		}
+		else if (!(isdigit(*str) || isalpha(*str)))
+		{
+			capitalize = 1;
+		}
 		str++;
-    }
+	}
 }
