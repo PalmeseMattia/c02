3a55671ed0092e00b0d115c3af49a1db7971aa58
Davide Mattia Palmese <dpalmese@e3r8p3.42roma.it>
2023-12-11 10:07:04 +0100

    added norm

--- ex03/ft_str_is_numeric.c
+++ ex03/ft_str_is_numeric.c
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dpalmese <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/11 10:05:47 by dpalmese          #+#    #+#             */
+/*   Updated: 2023/12/11 10:06:42 by dpalmese         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 int	ft_isdigit(int c)
 {
-    if (c >= '0' && c <= '9')
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
@@ -8,10 +20,11 @@ int	ft_isdigit(int c)
 
 int	ft_str_is_numeric(char *str)
 {
-	while(*str){
-		if(!ft_isdigit(*str))
+	while (*str)
+	{
+		if (!ft_isdigit(*str))
 			return (0);
 		str++;
 	}
-	return 0;
+	return (0);
 }
