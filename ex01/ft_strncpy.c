32ae47b9c9a44cc4660239ee8a5546d3d756fed6
Davide Mattia Palmese <dpalmese@e3r8p3.42roma.it>
2023-12-11 15:56:37 +0100

    added norm

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -6,13 +6,13 @@
 /*   By: dpalmese <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/11 10:03:02 by dpalmese          #+#    #+#             */
-/*   Updated: 2023/12/11 10:03:36 by dpalmese         ###   ########.fr       */
+/*   Updated: 2023/12/11 15:56:27 by dpalmese         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*strncpy(char *dest, const char *src, size_t n)
+char	*ft_strncpy(char *dest, const char *src, size_t n)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
 	while (i < n && src[i] != '\0')
